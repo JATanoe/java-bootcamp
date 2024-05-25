41635830359c7462a967b12385b0a5a97acf362b
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-05-25 14:34:27 -0500

    Week 03 - Lesson 4: Methods

--- week03/src/com/coderscampus/week03/Human.java
+++ week03/src/com/coderscampus/week03/Human.java
@@ -1,8 +1,16 @@
 package com.coderscampus.week03;
 
 public class Human {
+	String name;
+	String occupation;
 	String gender;
 	String height;
 	String weight;
 	String eyeColor;
-}
+	
+	// (Visibility modifier), return type, method name, parameters
+	String getInfo () {
+		String info = "\nName: " + name + "\nGender: " + gender + "\nHeight: " + height + " ft \nWeight: " + weight + " lbs \nEye color: " + eyeColor + "\nOccupation: " + occupation;
+		return info;
+	}
+} 
--- week03/src/com/coderscampus/week03/RunnableClass.java
+++ week03/src/com/coderscampus/week03/RunnableClass.java
@@ -8,21 +8,36 @@ public class RunnableClass {
 		
 		Human jeanGrah = new Human();
 		
+		jeanGrah.name = "Jean Grah";
+		jeanGrah.occupation = "Full Stack Developer";
 		jeanGrah.gender = "Male";
 		jeanGrah.height = "6'0\"";
 		jeanGrah.weight = "195";
 		jeanGrah.eyeColor = "Brown";
 		
-		System.out.println(jeanGrah);
+		System.out.println(jeanGrah.getInfo());
 		
 		Human elonMusk = new Human();
+
+		elonMusk.name = "Elon Musk";
+		elonMusk.occupation = "Businessman and investor";
+		elonMusk.gender = "Male";
+		elonMusk.height = "5'10\"";
+		elonMusk.weight = "195";
+		elonMusk.eyeColor = "Blue";
 		
-		jeanGrah.gender = "Male";
-		jeanGrah.height = "5'10\"";
-		jeanGrah.weight = "195";
-		jeanGrah.eyeColor = "Blue";
+		System.out.println(elonMusk.getInfo());
 		
-		System.out.println(elonMusk);
+		Human helenMirren = new Human();
+
+		helenMirren.name = "Helen Mirren";
+		helenMirren.occupation = "Actress";
+		helenMirren.gender = "Female";
+		helenMirren.height = "4'7\"";
+		helenMirren.weight = "156";
+		helenMirren.eyeColor = "Gray";
+		
+		System.out.println(helenMirren.getInfo());
 	}
 
 }
