53b5dbb0ea3f6089d4c98dc995bf422eec93dac9
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-05-25 01:18:03 -0500

    Week 03 - Lesson 3: Object Dot Properties

--- week03/src/com/coderscampus/week03/RunnableClass.java
+++ week03/src/com/coderscampus/week03/RunnableClass.java
@@ -7,6 +7,22 @@ public class RunnableClass {
 		// Object -> a Noun, it's a instantiation of a Class
 		
 		Human jeanGrah = new Human();
+		
+		jeanGrah.gender = "Male";
+		jeanGrah.height = "6'0\"";
+		jeanGrah.weight = "195";
+		jeanGrah.eyeColor = "Brown";
+		
+		System.out.println(jeanGrah);
+		
+		Human elonMusk = new Human();
+		
+		jeanGrah.gender = "Male";
+		jeanGrah.height = "5'10\"";
+		jeanGrah.weight = "195";
+		jeanGrah.eyeColor = "Blue";
+		
+		System.out.println(elonMusk);
 	}
 
 }
