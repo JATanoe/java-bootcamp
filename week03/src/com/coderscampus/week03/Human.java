3f8176b9c244b6e93e0b6d19c4edcd7136c79edd
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-05-25 18:25:17 -0500

    Week 03 - Lesson 5: Methods Inputs

--- week03/src/com/coderscampus/week03/Human.java
+++ week03/src/com/coderscampus/week03/Human.java
@@ -13,4 +13,21 @@ public class Human {
 		String info = "\nName: " + name + "\nGender: " + gender + "\nHeight: " + height + " ft \nWeight: " + weight + " lbs \nEye color: " + eyeColor + "\nOccupation: " + occupation;
 		return info;
 	}
+	
+	void setProperties (String name, String gender, String height, String weight, String eyeColor, String occupation) {
+		this.name = name;
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
+		this.eyeColor = eyeColor;
+		this.occupation = occupation;
+		
+		System.out.println("Name: " + this.name);
+		System.out.println("Gender: " + this.gender);
+		System.out.println("Height: " + this.height + " ft ");
+		System.out.println("Weight: " + this.weight + " lbs ");
+		System.out.println("Eye color: " + this.eyeColor);
+		System.out.println("Occupation: " + this.occupation);
+		System.out.println();
+	}
 } 
--- week03/src/com/coderscampus/week03/RunnableClass.java
+++ week03/src/com/coderscampus/week03/RunnableClass.java
@@ -1,43 +1,22 @@
 package com.coderscampus.week03;
 
 public class RunnableClass {
-
+ 
 	public static void main(String[] args) {		
 		// Class -> Blueprint for an Object
 		// Object -> a Noun, it's a instantiation of a Class
 		
 		Human jeanGrah = new Human();
 		
-		jeanGrah.name = "Jean Grah";
-		jeanGrah.occupation = "Full Stack Developer";
-		jeanGrah.gender = "Male";
-		jeanGrah.height = "6'0\"";
-		jeanGrah.weight = "195";
-		jeanGrah.eyeColor = "Brown";
-		
-		System.out.println(jeanGrah.getInfo());
+		jeanGrah.setProperties("Jean Grah", "Full Stack Developer", "Male", "6'0\"", "195", "Brown");
 		
 		Human elonMusk = new Human();
 
-		elonMusk.name = "Elon Musk";
-		elonMusk.occupation = "Businessman and investor";
-		elonMusk.gender = "Male";
-		elonMusk.height = "5'10\"";
-		elonMusk.weight = "195";
-		elonMusk.eyeColor = "Blue";
-		
-		System.out.println(elonMusk.getInfo());
+		elonMusk.setProperties("Elon Musk", "Businessman and investor", "Male", "5'10\"", "195", "Blue");
 		
 		Human helenMirren = new Human();
 
-		helenMirren.name = "Helen Mirren";
-		helenMirren.occupation = "Actress";
-		helenMirren.gender = "Female";
-		helenMirren.height = "4'7\"";
-		helenMirren.weight = "156";
-		helenMirren.eyeColor = "Gray";
-		
-		System.out.println(helenMirren.getInfo());
+		helenMirren.setProperties("Helen Mirren", "Actress", "Female", "4'7\"", "156", "Gray");
 	}
 
 }
