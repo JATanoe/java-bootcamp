d267959d5c098ff450ce3bbd13959b0caaedaff4
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-05-26 09:56:04 -0500

    Unit 03 - Lesson 6: For Loops

---
+++ week03/src/com/coderscampus/week03/Loops.java
@@ -0,0 +1,14 @@
+package com.coderscampus.week03;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		// Initialization, termination condition, increment/decrement
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
