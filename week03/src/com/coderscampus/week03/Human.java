dd89e2eee846f2e4a6583cf54874e0943c3bb3d4
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-05-27 21:21:57 -0500

    Unit 03 - Lesson 10: Constructors

--- week03/src/com/coderscampus/week03/Human.java
+++ week03/src/com/coderscampus/week03/Human.java
@@ -1,6 +1,7 @@
 package com.coderscampus.week03;
 
 public class Human {
+	
 	String name;
 	String occupation;
 	String gender;
@@ -8,26 +9,22 @@ public class Human {
 	String weight;
 	String eyeColor;
 	
-	// (Visibility modifier), return type, method name, parameters
-	String getInfo () {
-		String info = "\nName: " + name + "\nGender: " + gender + "\nHeight: " + height + " ft \nWeight: " + weight + " lbs \nEye color: " + eyeColor + "\nOccupation: " + occupation;
-		return info;
-	}
+	Human () {}
 	
-	void setProperties (String name, String gender, String height, String weight, String eyeColor, String occupation) {
+	Human (String name, String gender, String height, String weight, String eyeColor, String occupation) {
 		this.name = name;
 		this.gender = gender;
 		this.height = height;
 		this.weight = weight;
 		this.eyeColor = eyeColor;
 		this.occupation = occupation;
-		
-		System.out.println("Name: " + this.name);
-		System.out.println("Gender: " + this.gender);
-		System.out.println("Height: " + this.height + " ft ");
-		System.out.println("Weight: " + this.weight + " lbs ");
-		System.out.println("Eye color: " + this.eyeColor);
-		System.out.println("Occupation: " + this.occupation);
-		System.out.println();
 	}
+	
+	// (Visibility modifier), return type, method name, parameters
+	String getInfo () {
+		return "Name: " + name + "\nGender: " + gender 
+				+ "\nHeight: " + height + " ft \nWeight: " + weight 
+				+ " lbs \nEye color: " + eyeColor + "\nOccupation: " + occupation + "\n";
+	}
+	
 } 
--- week03/src/com/coderscampus/week03/RunnableClass.java
+++ week03/src/com/coderscampus/week03/RunnableClass.java
@@ -8,23 +8,24 @@ public class RunnableClass {
 		// Class -> Blueprint for an Object
 		// Object -> a Noun, it's a instantiation of a Class
 		
-		Random random = new Random();
+//		Random random = new Random();
+//		
+//		for (int i = 0; i < 20; i++) {
+//			System.out.println("Random number is " + random.nextInt(10));
+//		}
 		
-		for (int i = 0; i < 20; i++) {
-			System.out.println("Random number is " + random.nextInt(10));
-		}
+		// Call no args constructor
+		Human anotherHuman = new Human();
 		
-//		Human jeanGrah = new Human();
-//		
-//		jeanGrah.setProperties("Jean Grah", "Full Stack Developer", "Male", "6'0\"", "195", "Brown");
-//		
-//		Human elonMusk = new Human();
-//
-//		elonMusk.setProperties("Elon Musk", "Businessman and investor", "Male", "5'10\"", "195", "Blue");
-//		
-//		Human helenMirren = new Human();
-//
-//		helenMirren.setProperties("Helen Mirren", "Actress", "Female", "4'7\"", "156", "Gray");
+		// Call agrs constructor
+		Human jeanGrah = new Human("Jean Grah", "Full Stack Developer", "Male", "6'0\"", "195", "Brown");
+		System.out.println(jeanGrah.getInfo());
+		
+		Human elonMusk = new Human("Elon Musk", "Businessman and investor", "Male", "5'10\"", "195", "Blue");
+		System.out.println(elonMusk.getInfo());
+		
+		Human helenMirren = new Human("Helen Mirren", "Actress", "Female", "4'7\"", "156", "Gray");
+		System.out.println(helenMirren.getInfo());
 		
 		/*
 		 * UNIT 3 - [Exercise]: Creating a Method
